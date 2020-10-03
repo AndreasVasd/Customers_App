38dc8a3c5f69734d61b2192833c2df8bc3603c21
Andreas Vasd <andreas_1996@windowslive.com>
2020-10-03 18:58:36 +0300

    get request works!

--- Customers/src/app/customers/customers.page.ts
+++ Customers/src/app/customers/customers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CustomersService } from '../customers.service';
-import { HttpClient } from "@angular/common/http";
+//import { CustomersService } from '../customers.service';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { threadId } from 'worker_threads';
+//import { Customer } from '../customers/customers.model';
 
 
 @Component({
@@ -9,24 +11,31 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./customers.page.scss'],
 })
 export class CustomersPage implements OnInit {
-  customers = [];
-
-
-  constructor(private http: HttpClient) { 
-    this.http.get('http://northwind.netcore.io/customers.json').toPromise().then(data => {
-      console.log(data);
-
-      for(let key in data)
-        if(data.hasOwnProperty(key))
-        this.customers.push(data[key]);
-    });
-  }
-   
-
-    
+  //customers = [];
   
 
+  constructor(private http: HttpClient) { }
+    
+  customersArray= [];
+  //obj: any;
+  //data: any;
+
+  
   ngOnInit() {
+   return this.http.get('./assets/customers.json').subscribe(
+      data => {
+         for (let key in data['customers']) //data = eksoteriko object customers=array.. key = index
+         //if(data.hasOwnProperty(key))
+           //console.log(data[key]); 
+         this.customersArray.push(data['customers'][key]); //key is the index
+         console.log(this.customersArray);
+         //console.log(this.customers[0][0].id);
+      },
+      
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
     /*
     this.customersService.getData().subscribe();*/
   }
