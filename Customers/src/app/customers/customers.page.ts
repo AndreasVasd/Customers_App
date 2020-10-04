cd3eadcf40c339a3f3b3286ae6c1ef4d9eba1b38
Andreas Vasd <andreas_1996@windowslive.com>
2020-10-04 14:27:32 +0300

    Display data correctly

--- Customers/src/app/customers/customers.page.ts
+++ Customers/src/app/customers/customers.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import { CustomersService } from '../customers.service';
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { threadId } from 'worker_threads';
-//import { Customer } from '../customers/customers.model';
+
 
 
 @Component({
@@ -11,33 +9,26 @@ import { threadId } from 'worker_threads';
   styleUrls: ['./customers.page.scss'],
 })
 export class CustomersPage implements OnInit {
-  //customers = [];
   
-
   constructor(private http: HttpClient) { }
     
+  //Initialize my custom customers array
   customersArray= [];
-  //obj: any;
-  //data: any;
 
   
   ngOnInit() {
+   //Send the GET request
    return this.http.get('./assets/customers.json').subscribe(
-      data => {
-         for (let key in data['customers']) //data = eksoteriko object customers=array.. key = index
-         //if(data.hasOwnProperty(key))
-           //console.log(data[key]); 
-         this.customersArray.push(data['customers'][key]); //key is the index
-         console.log(this.customersArray);
-         //console.log(this.customers[0][0].id);
+      dataRes => {
+         for (let key in dataRes['customers']) //dataRes = eksoteriko object(response of request), customers=array of objects.. key = index
+          //console.log(data['customers']);
+          this.customersArray.push(dataRes['customers'][key]); //key is the index
+          //console.log(this.customersArray);
       },
-      
       (err: HttpErrorResponse) => {
         console.log(err.message);
       }
     );
-    /*
-    this.customersService.getData().subscribe();*/
   }
 
 }
