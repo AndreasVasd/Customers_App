55221b01681cab5cf248eb963d3b7da4ec2cba0b
Andreas Vasd <andreas_1996@windowslive.com>
2020-10-04 19:37:36 +0300

    Finish :)

--- Customers/src/app/customers.service.ts
+++ Customers/src/app/customers.service.ts
@@ -1,20 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersService {
 
-  customers = [];
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
-  getData() {
-    return this.http.get(
-      'http://northwind.servicestack.net/customers.json').toPromise().then(resData => {
-        console.log(resData);
-      });
-  }
 
 }
--- Customers/src/app/customers/customers.page.ts
+++ Customers/src/app/customers/customers.page.ts
@@ -9,18 +9,25 @@ import { HttpClient, HttpErrorResponse } from "@angular/common/http";
   styleUrls: ['./customers.page.scss'],
 })
 export class CustomersPage implements OnInit {
-  
-  constructor(private http: HttpClient) { }
-    
-  //Initialize my custom customers array
-  customersArray= [];
 
+
+  constructor(private http: HttpClient,) { }
+    
+ 
+  Subscriber: any;
+  customersArray= [];
+  
+
+  //For filtering
+  searchItem: string = "";
+  filterCus= [];
   
   ngOnInit() {
+
    //Send the GET request
-   return this.http.get('./assets/customers.json').subscribe(
+    this.Subscriber = this.http.get('./assets/customers.json').subscribe(
       dataRes => {
-         for (let key in dataRes['customers']) //dataRes = eksoteriko object(response of request), customers=array of objects.. key = index
+         for (let key in dataRes['customers']) 
           //console.log(data['customers']);
           this.customersArray.push(dataRes['customers'][key]); //key is the index
           //console.log(this.customersArray);
@@ -28,7 +35,19 @@ export class CustomersPage implements OnInit {
       (err: HttpErrorResponse) => {
         console.log(err.message);
       }
-    );
+    );  
+
+    this.filterCus = this.customersArray;
+    return this.Subscriber;
+
   }
+
+  //Filtering
+  filterCustomers(searchItem) { 
+       this.filterCus = this.customersArray.filter(customer => { 
+      return customer.contactName.indexOf(searchItem) > -1; 
+    }); 
+  }
+ 
 
 }
